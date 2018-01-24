834544f22bdf1099bbdc14dc52ebc956a36bb81f
Lex <alexisjaynedefoe@gmail.com>
2018-01-24 13:33:26 +0000

    :space_invader:health bar and typeface

--- src/components/select-character-form/index.js
+++ src/components/select-character-form/index.js
@@ -6,45 +6,50 @@ import tama3 from "../../../public/tamagotchi3.png";
 export class CharacterForm extends React.Component {
   render() {
     return (
-      <form onSubmit={this.props.handleSubmit}>
-        <label htmlFor="tamagotchi1">
-          <img src={tama1} />
-        </label>
-        <input
-          type="radio"
-          id="tamagotchi1"
-          name="tamagotchi"
-          value="tamagotchi1"
-          checked={this.props.radioSelected === "tamagotchi1"}
-          onChange={this.props.handleChange}
-        />
+      <div>
+        <div className="start__message">
+          <p className="choose">CHOOSE ONE</p>
+        </div>
+        <form onSubmit={this.props.handleSubmit}>
+          <label htmlFor="tamagotchi1">
+            <img src={tama1} />
+          </label>
+          <input
+            type="radio"
+            id="tamagotchi1"
+            name="tamagotchi"
+            value="tamagotchi1"
+            checked={this.props.radioSelected === "tamagotchi1"}
+            onChange={this.props.handleChange}
+          />
 
-        <label htmlFor="tamagotchi2">
-          <img src={tama2} />
-        </label>
-        <input
-          type="radio"
-          id="tamagotchi2"
-          name="tamagotchi"
-          value="tamagotchi2"
-          checked={this.props.radioSelected === "tamagotchi2"}
-          onChange={this.props.handleChange}
-        />
+          <label htmlFor="tamagotchi2">
+            <img src={tama2} />
+          </label>
+          <input
+            type="radio"
+            id="tamagotchi2"
+            name="tamagotchi"
+            value="tamagotchi2"
+            checked={this.props.radioSelected === "tamagotchi2"}
+            onChange={this.props.handleChange}
+          />
 
-        <label htmlFor="tamagotchi3">
-          <img src={tama3} />
-        </label>
-        <input
-          type="radio"
-          id="tamagotchi3"
-          name="tamagotchi"
-          value="tamagotchi3"
-          checked={this.props.radioSelected === "tamagotchi3"}
-          onChange={this.props.handleChange}
-        />
+          <label htmlFor="tamagotchi3">
+            <img src={tama3} />
+          </label>
+          <input
+            type="radio"
+            id="tamagotchi3"
+            name="tamagotchi"
+            value="tamagotchi3"
+            checked={this.props.radioSelected === "tamagotchi3"}
+            onChange={this.props.handleChange}
+          />
 
-        <button type="submit">Submit</button>
-      </form>
+          <button type="submit">ENTER</button>
+        </form>
+      </div>
     );
   }
 }
--- src/components/tamagotchi/index.js
+++ src/components/tamagotchi/index.js
@@ -1,10 +1,16 @@
 import React from "react";
 import HealthBar from "./HealthBar";
-import tama1 from "../../../public/tamagotchi1trans.png";
+// import SpriteCanvas from "./SpriteCanvas";
+// import tama1 from "../../../public/tamagotchi1trans.png";
 import "./style.css";
 
 export class Tamagotchi extends React.Component {
   render() {
-    return <HealthBar />;
+    return (
+      <div>
+        <HealthBar />
+        {/* <SpriteCanvas tamagotchi={this.props.radioSelected} /> */}
+      </div>
+    );
   }
 }
